c9d0394e15b13459cb13356d4fe7dc936e0c4d0f
ET|冰琳 <binglin@uoko.com>
2017-05-18 18:39:10 +0800

    prepare to analyze yml

--- JSONConverter/Swagger/SwaggerObject.swift
+++ JSONConverter/Swagger/SwaggerObject.swift
@@ -8,25 +8,51 @@
 
 import Foundation
 
-struct SwaggerObject {
-    var swagger: String
+// ----------------------------------------------------------------
+struct SwaggerMainInfo {
+    
+    var paths: [PathItemObject]
+    var parameters: [ParameterObject]
+    var definitions: [DefinitionObject]
+}
+
+struct SwaggerPathObject {
+    var path: String
+    var method: String
+    var operation: OperationObject?
+}
+
+// 全局parameters
+struct SwaggerParameterObject {
+    var key: String
+    var parameter: ParameterObject
+}
+
+
+// ----------------------------------------------------------------
+struct SwaggerObject: Mapping {
+    var swagger: String?
     var info: [InfoObject] = []
     var host: String?
     var basePath: String?
     var schemes: [String]?
     var consumes: [String]?
     var produces: [String]?
-    var paths: [PathObject]
-    var definitions: [DefinitionObject]
-    var parameters: [ParameterDefinitionObject]
-    var responses: [ResponseDefinitionObject]
-    var securityDefinitions: [SecurityDefinitionObject]
-    var security: [SecurityRequirementObject]
-    var tags: [TagObject]
-    var externalDocs: ExternalDocumentationObject
+
+    //   var paths: [PathObject]
+//    var definitions: [DefinitionObject]
+//    var parameters: [ParameterDefinitionObject]
+    
+//    var securityDefinitions: [SecurityDefinitionObject]
+//    var security: [SecurityRequirementObject]
+    var tags: [TagObject]?
+    var externalDocs: ExternalDocumentationObject?
+    
+    var pathObjects: [SwaggerPathObject]?
+    var parameterObjects: [SwaggerParameterObject]?
 }
 
-struct InfoObject {
+struct InfoObject: Mapping {
     var title: String
     var description: String?
     var termsOfService: String?
@@ -43,11 +69,7 @@ struct InfoObject {
 }
 
 
-struct PathObject {
-    
-}
-
-struct PathItemObject {
+struct PathItemObject: Mapping {
     var ref: String
     var get: OperationObject?
     var put: OperationObject?
@@ -60,15 +82,36 @@ struct PathItemObject {
     
 }
 
-struct OperationObject {
+struct OperationObject: Mapping {
+    var tags: [String]?
+    var summary: String?
+    var description: String?
+    var externalDocs: ExternalDocumentationObject?
+    var operationId: String?
+    var consumes: [String]?
+    var produces: [String]?
+    
+    var parameters: [ParameterObject]
+    //var response: ResponseObject
+    
+    var schemes: [String]?
+    var deprecated: Bool?
     
 }
 
-struct DefinitionObject {
-    
+struct DefinitionObject: Mapping {
+    var ref: String?
+    var title: String?
+    var description: String?
+    var `enum`: [String]?
+    var required: Bool?
+    var items: [String]?
+    var discriminator: String?
+    var type: ItemObjectType?
+    var format: String?
 }
 
-enum ParameterIn: String {
+enum ParameterIn: String, EnumString {
     case query = "query"
     case header = "header"
     case path = "path"
@@ -76,7 +119,7 @@ enum ParameterIn: String {
     case body = "body"
 }
 
-enum ParameterType: String {
+enum ParameterType: String, EnumString {
     case string = "string"
     case number = "number"
     case integer = "integer"
@@ -85,7 +128,7 @@ enum ParameterType: String {
     case file    = "file"
 }
 
-struct SchemaObject {
+struct SchemaObject: Mapping {
     var ref: String?// - As a JSON Reference
     var format: String? // (See Data Type Formats for further details)
     var title: String?//
@@ -109,7 +152,7 @@ struct SchemaObject {
     var type: String?
 }
 
-enum ItemObjectType: String {
+enum ItemObjectType: String, EnumString {
     case string = "string"
     case number = "number"
     case integer = "integer"
@@ -117,41 +160,11 @@ enum ItemObjectType: String {
     case array   = "array"
 }
 
-struct ItemObject {
+struct ItemObject: Mapping {
     var type: ItemObjectType
     var format: String?
     var items: [ItemObject]?
     var collectionFormat: [String]?
-    var `enum`: [String]?
-    
-}
-
-struct ParameterObject {
-    var name: String
-    var `in`: ParameterIn
-    var description: String?
-    var required: Bool
-    
-    //if inbody
-    var schema: SchemaObject?
-    
-    //if in body
-    var type: ParameterType
-    var formate: String?
-    var allowEmptyValue: Bool?
-    
-    // type is array? is required
-    var items: [ItemObject]?
-    
-    /** required if in query or formData
-     * 
-     csv - comma separated values foo,bar.
-     ssv - space separated values foo bar.
-     tsv - tab separated values foo\tbar.
-     pipes - pipe separated values foo|bar.
-     multi - c
-     */
-    var collectionFormat: String? //
     
     var `default`: Any
     
@@ -170,45 +183,85 @@ struct ParameterObject {
     
 }
 
-struct ParameterDefinitionObject {
+struct ParameterObject: Mapping, KeyMapping {
+    var ref: String?
+    var name: String?
+    var `in`: ParameterIn?
+    var description: String?
+    var required: Bool?
+    
+    //if inbody
+    var schema: SchemaObject?
+    
+    //if in body
+    var type: ParameterType?
+    var formate: String?
+    var allowEmptyValue: Bool?
+    
+    // type is array? is required
+    var items: [ItemObject]?
+    
+    /** required if in query or formData
+     * 
+     csv - comma separated values foo,bar.
+     ssv - space separated values foo bar.
+     tsv - tab separated values foo\tbar.
+     pipes - pipe separated values foo|bar.
+     multi - c
+     */
+    var collectionFormat: String? //
+    
+    //var `default`: Any
+    
+    var maxinum: Int?
+    var exclusiveMaximum: Bool?
+    var minimum: Int?
+    var exclusiveMinimum: Bool?
+    var maxLength: Int?
+    var minLength: Int?
+    var pattern: String?
+    var maxItems: Int?
+    var minItems: Int?
+    var uniqueItems: Bool?
+    var `enum`: [String]?
+    var multipleOf: Int?
+    
+    static func mappedKeyFor(key: String) -> String? {
+        
+        if key == "ref" {
+            return "$ref"
+        }
+        return nil
+    }
+}
+
+struct ParameterDefinitionObject: Mapping {
     //{name}	Parameter Object	A single parameter definition, mapping a "name" to the parameter it defines.
     
     var key: String
     var parameter: ParameterObject
 }
 
-struct ResponseDefinitionObject {
-    
-}
-
-struct SecurityDefinitionObject {
-    
-}
-
-struct SecurityRequirementObject {
-    
-}
-
-struct TagObject {
+struct TagObject: Mapping {
     var name: String
     var description: String?
     var externalDocs: ExternalDocumentationObject?
     
 }
 
-struct ExternalDocumentationObject {
+struct ExternalDocumentationObject: Mapping {
     var description: String?
     var url: String
 }
 
 
-struct ContactObject {
+struct ContactObject: Mapping {
     var name: String?
     var url: String?
     var email: String?
 }
 
-struct LicenseObject {
+struct LicenseObject: Mapping {
     var name: String
     var url: String?
 }
--- JSONConverter/ViewController.swift
+++ JSONConverter/ViewController.swift
@@ -20,6 +20,87 @@ class ViewController: NSViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        
+        transfer.target = self
+        transfer.action = #selector(transferAction)
+        
+        transferYml()
+    }
+    
+    func transferYml() {
+        let url = Bundle.main.path(forResource: "swagger", ofType: "json")
+        
+        if let url = url {
+            
+            print(url)
+            
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: url))
+                let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
+                //print(json)
+                
+                guard let dic = json as? [String : Any] else { return }
+                
+                var swagger = SwaggerObject(fromDic: dic)
+                
+                guard let paths = dic["paths"] as? [String: [String: Any]] else { return }
+                
+               
+                // paths
+                var pathObjects: [SwaggerPathObject] = []
+                
+                for (key, value) in paths {
+                    
+                    let path = key
+                    
+                    let method = value.keys.first!
+                    
+                    var pathObject = SwaggerPathObject(path: path, method: method, operation: nil)
+                    
+                    if let opertionObject = value[method] as? [String: Any] {
+                        
+                        let operate = OperationObject(fromDic: opertionObject)
+                        pathObject.operation = operate
+                        
+                        print("path:", path, "  method", method)
+                    }
+                    pathObjects.append(pathObject)
+                }
+                
+                swagger.pathObjects = pathObjects
+                
+
+                // global parameters
+                var sparameterObj: [SwaggerParameterObject] = []
+                if let parameters = dic["parameters"] as? [String: [String: Any]] {
+                    
+                    for (key, value) in parameters {
+                        
+                        let parameterObj = ParameterObject(fromDic: value)
+                        
+                        let obj = SwaggerParameterObject(key: key, parameter: parameterObj)
+                        sparameterObj.append(obj)
+                    }
+                }
+                swagger.parameterObjects = sparameterObj
+                
+                
+                
+                //definitions 里的entity 定义
+                
+                
+                
+                //definition  api response里的entity
+                
+                
+            }catch (let e) {
+                print(e)
+            }
+        }
+    }
+    
+    func transferJSONMode() {
+        
         let url = Bundle.main.path(forResource: "JSONModel", ofType: "json")
         if let url = url {
             print(url)
@@ -40,9 +121,7 @@ class ViewController: NSViewController {
                 print(e)
             }
         }
-        
-        transfer.target = self
-        transfer.action = #selector(transferAction)
+    
     }
     
     func transferAction() {
